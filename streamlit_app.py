e38c50c8113707c752ae20568e428ccbeaee2303
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-03-26 20:41:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,4 +89,4 @@ if archivo:
             tabla_html = tabla_resultados.to_html(index=False)
             st.write(f'<h2>Resultados:</h2>{tabla_html}', unsafe_allow_html=True, target='new')
         else:
-            st.write("No se encontraron resultados")
+            st.write("No se encontraron resultados") 
